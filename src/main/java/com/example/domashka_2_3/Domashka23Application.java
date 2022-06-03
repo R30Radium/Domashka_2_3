f5a94837181540e52a049903dc929ac535071b32
R30Radium <r30radium@gmail.ru>
2022-06-03 05:40:26 +0400

    kommmmmit

---
+++ src/main/java/com/example/domashka_2_3/Domashka23Application.java
@@ -0,0 +1,13 @@
+package com.example.domashka_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Domashka23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Domashka23Application.class, args);
+    }
+
+}
